9d71f97ec73e6254ee5e460dca47e05c2ebed68c
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-16 17:52:34 +0200

    Added print to DOM for equal result

--- js/myscript.js
+++ js/myscript.js
@@ -1,27 +1,42 @@
 console.log("JS Loaded Succesfully!");
 
+const containerBlocks = document.getElementById('result-container');
+
 for ( let i = 1 ; i < 101 ; i++){
 
-    //Print From 1 to 100 on console
-    // DO NOT TOUCH THE NEXT LINE
-        // # console.log(i);
+    const multipleBlock = document.createElement('span');
+    const fizzBuzz = 'FizzBuzz';
+    const fizz = 'Fizz';
+    const buzz = 'Buzz';
 
     // If cycle
 
         //Check if is mutiple with 3 and 5
         if (i % 3 === 0 && i % 5 === 0)  {
-        console.log('(' + i + ') ' + 'FizzBuzz'); 
+
+            console.log('(' + i + ') ' + fizzBuzz); 
+            multipleBlock.innerHTML = i + fizzBuzz;
+
         } //Check if is mutiple with 3
         else if (i % 3 === 0) {
-            console.log('(' + i + ') ' + 'Fizz');
+
+            console.log('(' + i + ') ' + fizz);
+            multipleBlock.innerHTML = i + fizz;
+
         } //Check if is mutiple with 5
         else if (i % 5 === 0) {
-            console.log('(' + i + ') ' + 'Buzz');
+
+            console.log('(' + i + ') ' + buzz);
+            multipleBlock.innerHTML = i + buzz;
+
         }  //Else return a normal value of the list
         else {
-            console.log(i); 
+            console.log(i);
+            multipleBlock.innerHTML = i;
         }
-}
+
+        containerBlocks.appendChild(multipleBlock);
+}   
 
 
 
