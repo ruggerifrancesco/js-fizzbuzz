ab518e3ae1613df27de94f9e246e617d4aef3d62
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-16 16:41:02 +0200

    Defined first JS Excercise structure and checks

---
+++ js/myscript.js
@@ -0,0 +1,24 @@
+console.log("JS Loaded Succesfully!");
+
+for ( let i = 1 ; i < 101 ; i++){
+    
+    //Print From 1 to 100 on console
+    // DO NOT TOUCH THE NEXT LINE
+        // # console.log(i);
+
+    // If cycle
+    if (i % 3 === 0) {
+        console.log(i + ' M3');
+    } else if (i % 5 === 0) {
+        console.log(i + ' M5');
+    } else {
+        console.log(i); 
+    }
+}
+
+
+
+
+
+
+
