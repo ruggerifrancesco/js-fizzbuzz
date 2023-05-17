562a9df7bf3554a915993ebbd614c128254321bf
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-17 10:16:35 +0200

    commit

--- js/myscript.js
+++ js/myscript.js
@@ -14,20 +14,20 @@ for ( let i = 1 ; i < 101 ; i++){
         //Check if is mutiple with 3 and 5
         if (i % 3 === 0 && i % 5 === 0)  {
 
-            console.log('(' + i + ') ' + fizzBuzz); 
-            multipleBlock.innerHTML = i + fizzBuzz;
+            console.log(fizzBuzz); 
+            multipleBlock.innerHTML = fizzBuzz;
 
         } //Check if is mutiple with 3
         else if (i % 3 === 0) {
 
-            console.log('(' + i + ') ' + fizz);
-            multipleBlock.innerHTML = i + fizz;
+            console.log(fizz);
+            multipleBlock.innerHTML = fizz;
 
         } //Check if is mutiple with 5
         else if (i % 5 === 0) {
 
-            console.log('(' + i + ') ' + buzz);
-            multipleBlock.innerHTML = i + buzz;
+            console.log(buzz);
+            multipleBlock.innerHTML = buzz;
 
         }  //Else return a normal value of the list
         else {
